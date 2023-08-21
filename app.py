5d603748f6715989d20fa4df61395c9e6fdb61ad
Mhanjhusriee Baskar <mhanjhu15@gmail.com>
2023-08-21 17:56:19 +0530

    new

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 import streamlit as st
-import cv2
+import pandas as pd
 import numpy as np
+
 
 st.title('Car windshild defect detection')
 uploaded_file = st.file_uploader("Choose a image file", type=["jpg","jpeg"])
-if uploaded_file is not None:
-    # Convert the file to an opencv image.
-    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    opencv_image = cv2.imdecode(file_bytes, 1)
-
-    # Now do something with the image! For example, let's display it:
-    st.image(opencv_image, channels="BGR")
