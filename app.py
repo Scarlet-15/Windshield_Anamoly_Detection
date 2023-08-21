888ab8c7049e340889ee9c84f585189d9471bdf7
Mhanjhusriee Baskar <mhanjhu15@gmail.com>
2023-08-21 10:34:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import cv2
+
+st.title('Car windshild defect detection')
+uploaded_file = st.file_uploader("Choose a image file", type=["jpg","jpeg"])
+if uploaded_file is not None:
+    # Convert the file to an opencv image.
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    opencv_image = cv2.imdecode(file_bytes, 1)
+
+    # Now do something with the image! For example, let's display it:
+    st.image(opencv_image, channels="BGR")
